90948408b7cedcba657a90817a65d6736515da91
지원 <won_8014@naver.com>
2025-07-10 14:31:51 +0900

    잘 올라가는지 시도

--- slackVote/src/main/java/com/example/slackVote/Hello.java
+++ slackVote/src/main/java/com/example/slackVote/Hello.java
@@ -3,5 +3,6 @@ package com.example.slackVote;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
