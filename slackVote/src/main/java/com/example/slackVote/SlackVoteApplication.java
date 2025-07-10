17ca1cf01254c614787df99c9cd41a7c42198693
지원 <won_8014@naver.com>
2025-07-10 14:15:14 +0900

    Setting
    의존성 주입하고, 초기 오류 잡기

---
+++ slackVote/src/main/java/com/example/slackVote/SlackVoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.slackVote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackVoteApplication.class, args);
+	}
+
+}
---
+++ slackVote/src/test/java/com/example/slackVote/SlackVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slackVote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
